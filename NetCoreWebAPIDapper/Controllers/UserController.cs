03dd95c7e6485700f92802ad680250015b086621
Trần Văn Giao <nhatgiaoict@gmail.com>
2019-05-22 23:19:26 +0700

    user,role, function

--- NetCoreWebAPIDapper/Controllers/UserController.cs
+++ NetCoreWebAPIDapper/Controllers/UserController.cs
@@ -18,51 +18,53 @@ namespace NetCoreWebAPIDapper.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RoleController : ControllerBase
+    public class UserController : ControllerBase
     {
-        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
         private readonly string _connectionString;
-        public RoleController(RoleManager<AppRole> roleManager, IConfiguration configuration)
+        public UserController(UserManager<AppUser> userManager, IConfiguration configuration)
         {
-            _roleManager = roleManager;
+            _userManager = userManager;
             _connectionString = configuration.GetConnectionString("DbConnectionString");
         }
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            using (var conn = new SqlConnection(_connectionString))
+            using(var conn = new SqlConnection(_connectionString))
             {
                 if (conn.State == ConnectionState.Closed)
-                    conn.Open();
-                var result = await conn.QueryAsync<AppRole>("Get_Role_All", null, null, null, CommandType.StoredProcedure);
+                    await conn.OpenAsync();
+                var result = await conn.QueryAsync<AppUser>("Get_User_All", null, null, null, CommandType.StoredProcedure);
                 return Ok(result);
             }
         }
 
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
         {
-            return Ok(await _roleManager.FindByIdAsync(id));
+            return Ok(await _userManager.FindByIdAsync(id));
         }
 
         [HttpGet("paging")]
-        public async Task<IActionResult> GetPaging(string keyword, int pageIndex = 0, int pageSize = 10)
+        public async Task<IActionResult> GetPaging(string keyword, int pageIndex, int pageSize)
         {
             using (var conn = new SqlConnection(_connectionString))
             {
                 if (conn.State == ConnectionState.Closed)
                     await conn.OpenAsync();
+
                 var paramaters = new DynamicParameters();
                 paramaters.Add("@keyword", keyword);
                 paramaters.Add("@pageIndex", pageIndex);
                 paramaters.Add("@pageSize", pageSize);
                 paramaters.Add("@totalRow", dbType: DbType.Int32, direction: ParameterDirection.Output);
 
-                var result = await conn.QueryAsync<AppRole>("Get_Role_AllPaging", paramaters, null, null, CommandType.StoredProcedure);
+                var result = await conn.QueryAsync<AppUser>("Get_User_AllPaging", paramaters, null, null, CommandType.StoredProcedure);
 
                 int totalRow = paramaters.Get<int>("@totalRow");
 
-                var pagedResult = new PagedResult<AppRole>()
+                var pagedResult = new PagedResult<AppUser>()
                 {
                     Items = result.ToList(),
                     TotalRow = totalRow,
@@ -75,30 +77,30 @@ namespace NetCoreWebAPIDapper.Controllers
 
         [HttpPost]
         [ValidateModel]
-        public async Task<IActionResult> Post([FromBody] AppRole role)
+        public async Task<IActionResult>Post([FromBody] AppUser user)
         {
-            var result = await _roleManager.CreateAsync(role);
-            if (result.Succeeded)
-                return Ok(result);
-            return BadRequest();
-        }
-
-        [HttpPut]
-        [ValidateModel]
-        public async Task<IActionResult> Put([Required]Guid id, [FromBody] AppRole role)
-        {
-            role.Id = id;
-            var result = await _roleManager.UpdateAsync(role);
+            var result = await _userManager.CreateAsync(user);
             if (result.Succeeded)
                 return Ok();
             return BadRequest();
         }
 
-        [HttpDelete]
+        [HttpPut("{id}")]
+        [ValidateModel]
+        public async Task<IActionResult> Put([Required] Guid id, [FromBody] AppUser user)
+        {
+            user.Id = id;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+                return Ok();
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string id)
         {
-            var role = await _roleManager.FindByIdAsync(id);
-            var result = await _roleManager.DeleteAsync(role);
+            var user = await _userManager.FindByIdAsync(id);
+            var result = await _userManager.DeleteAsync(user);
             if (result.Succeeded)
                 return Ok();
             return BadRequest();
